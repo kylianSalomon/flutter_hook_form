c4af52ad08b7c954d219a70a28d3ef47f690078a
kylianSalomon <kylian.salomon@ripple-digital.fr>
2025-03-13 14:34:18 +0100

    🚚 : move code into "src" folder

--- lib/flutter_hook_form.dart
+++ lib/flutter_hook_form.dart
@@ -1,10 +1,8 @@
-// Export all public APIs
-
-export 'annotations/form_annotations.dart';
-export 'hooks/use_form.dart';
-export 'hooks/use_form_context.dart';
-export 'messages/form_messages.dart';
-export 'models/form_field_controller.dart';
-export 'models/form_schema.dart';
-export 'models/validator.dart';
-export 'validators/validators.dart';
+export 'src/annotations/form_annotations.dart';
+export 'src/hooks/use_form.dart';
+export 'src/hooks/use_form_context.dart';
+export 'src/messages/form_messages.dart';
+export 'src/models/form_field_controller.dart';
+export 'src/models/form_schema.dart';
+export 'src/models/validator.dart';
+export 'src/validators/validators.dart';
